0927c4c8e43b48210d800f5a9614151b4d225541
Devrath <devrath.dev595@gmail.com>
2024-02-27 11:34:58 +0530

    added new changes

--- Code/Xml/Sample1/app/src/androidTest/java/com/istudio/espresso/MainActivityTest.kt
+++ Code/Xml/Sample1/app/src/androidTest/java/com/istudio/espresso/MainActivityTest.kt
@@ -1,16 +1,14 @@
 package com.istudio.espresso
 
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.istudio.espresso.textmatch.MainActivity
 import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
--- Code/Xml/Sample1/app/src/main/java/com/istudio/espresso/textmatch/MainActivity.kt
+++ Code/Xml/Sample1/app/src/main/java/com/istudio/espresso/textmatch/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.istudio.espresso
+package com.istudio.espresso.textmatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.istudio.espresso.R
 
 class MainActivity : AppCompatActivity() {
 
