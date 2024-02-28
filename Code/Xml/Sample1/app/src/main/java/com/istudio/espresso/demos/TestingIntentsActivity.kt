2f119814dfb1a3447483d092febfd72024e90c22
Devrath <devrath.dev595@gmail.com>
2024-02-28 19:03:32 +0530

    added more code

--- Code/Xml/Sample1/app/src/main/java/com/istudio/espresso/demos/TestingIntentsActivity.kt
+++ Code/Xml/Sample1/app/src/main/java/com/istudio/espresso/demos/TestingIntentsActivity.kt
@@ -1,15 +1,19 @@
 package com.istudio.espresso.demos
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.view.View
 import android.widget.Toast
-import com.istudio.espresso.R
-import com.istudio.espresso.databinding.ActivityFormBinding
-import com.istudio.espresso.databinding.ActivityResourceIdlingBinding
-import com.istudio.espresso.databinding.ActivitySelectionBinding
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.istudio.espresso.databinding.ActivityTestingIntentsBinding
+import com.istudio.espresso.demos.helper_activities.ContactsActivity
+
 
 class TestingIntentsActivity : AppCompatActivity() {
 
@@ -19,11 +23,95 @@ class TestingIntentsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityTestingIntentsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
-        binding.dispBtnIntentId.setOnClickListener {
+    /**
+     * <**********************> ON-ACTIVITY Result  <**********************>
+     */
+    private val REQUEST_CODE_CALL_PHONE_PERMISSION = 17
+    // Declare the activity result launcher
+    private val pickContactLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val data = result.data
+            val resultCode = result.resultCode
 
+            if (resultCode == RESULT_OK) {
+                data?.extras?.let {
+                    val numberStr = it.getString(ContactsActivity.KEY_PHONE_NUMBER)
+                    binding.editTextCallerNumber.setText(numberStr)
+                }
+            }
+        }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_CALL_PHONE_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted, make the call
+                    onCall(null)
+                } else {
+                    // Permission denied, show a message or handle accordingly
+                    Toast.makeText(this, "Phone permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
     }
+    /**
+     * <**********************> ON-ACTIVITY Result  <**********************>
+     */
+
+    /**
+     * <**********************> ON-CLICK METHODS  <**********************>
+     */
+    // Button Action :-> OnClick of OnCall
+    fun onCall(view: View?) {
+        // Check if CALL-PHONE runtime permission is granted
+        val hasCallPhonePermission = checkIfCallPhonePermissionIsGranted()
+        if (hasCallPhonePermission) {
+            // Permission is available --> Call the PhoneActivity with Intent
+            startActivity(createCallIntentFromNumber())
+        } else {
+            // Request permission if not granted
+            requestCallPhonePermission()
+        }
+    }
+
+    // Button Action :-> OnClick of PickContact --> Launch the contact application
+    fun onPickContact(view: View?) {
+        // Start the activity for result using the activity result launcher
+        val pickContactIntent = Intent(this, ContactsActivity::class.java)
+        pickContactLauncher.launch(pickContactIntent)
+    }
+
+    /**
+     * <**********************> ON-CLICK METHODS  <**********************>
+     */
+
+    /**
+     * <**********************> HELPER METHODS  <**********************>
+     */
+    private fun checkIfCallPhonePermissionIsGranted() = ContextCompat.checkSelfPermission(
+        this, Manifest.permission.CALL_PHONE
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestCallPhonePermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE),
+            REQUEST_CODE_CALL_PHONE_PERMISSION
+        )
+    }
+
+    private fun createCallIntentFromNumber(): Intent {
+        val intentToCall = Intent(Intent.ACTION_CALL)
+        val number = binding.editTextCallerNumber.text.toString()
+        intentToCall.data = Uri.parse("tel:$number")
+        return intentToCall
+    }
+
+    /**
+     * <**********************> HELPER METHODS  <**********************>
+     */
 
 }
---
+++ Code/Xml/Sample1/app/src/main/java/com/istudio/espresso/demos/helper_activities/ContactsActivity.kt
@@ -0,0 +1,31 @@
+package com.istudio.espresso.demos.helper_activities
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.annotation.VisibleForTesting
+import com.istudio.espresso.R
+
+
+/**
+ * This a placeholder Activity for a contacts screen. This activity is never opened and does not
+ * contain any real contact data for keeping this sample simple and focused.
+ */
+class ContactsActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts)
+        setResult(RESULT_OK, createResultData("896-745-231"))
+        finish()
+    }
+
+    companion object {
+        const val KEY_PHONE_NUMBER = "key_phone_number"
+        @VisibleForTesting
+        fun createResultData(phoneNumber: String?): Intent {
+            val resultData = Intent()
+            resultData.putExtra(KEY_PHONE_NUMBER, phoneNumber)
+            return resultData
+        }
+    }
+}
