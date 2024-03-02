c4d5385803182efa4d930c38a70b99a28f7a3023
Devrath <devrath.dev595@gmail.com>
2024-03-02 23:54:10 +0530

    added

---
+++ Code/MockServer/app/src/main/java/com/istudio/mockwebserver/network/ApiService.kt
@@ -0,0 +1,18 @@
+package com.istudio.mockwebserver.network
+
+import com.istudio.mockwebserver.data.ServerResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+// https://raw.githubusercontent.com/devrath/Sample-Data/master/CurrencyAppSampleData/Currencies.json
+interface ApiService {
+
+    companion object {
+        private const val currency = "Currencies.json"
+    }
+
+    @GET(currency)
+    fun getPost(): Call<ServerResponse>
+
+}
+
