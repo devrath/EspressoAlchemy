744b6b2fe47bcc0960e00d4359dbdc043111fa6d
Devrath <devrath.dev595@gmail.com>
2024-03-04 22:47:44 +0530

    added

--- Code/ComposeTesting/app/src/androidTest/java/com/istudio/compose_testing/MainActivityTest.kt
+++ Code/ComposeTesting/app/src/androidTest/java/com/istudio/compose_testing/MainActivityTest.kt
@@ -1,15 +1,8 @@
 package com.istudio.compose_testing
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import com.istudio.compose_testing.modules.DisplayButtonDemo
-import com.istudio.compose_testing.ui.theme.ComposetestingTheme
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 
@@ -22,6 +15,5 @@ class MainActivityTest {
     fun myTest() {
         composeTestRule.onNodeWithText("Hello World!").performClick()
     }
-
-
+    
 }
